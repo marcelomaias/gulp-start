427bb0a258211883b6c53aaa8e4451f3c90e7e9d
Maia <maia@Citadel.local>
2019-11-04 21:13:25 -0300

    Added Box example

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,3 +2,10 @@ import User from "./inc/component.js";
 
 const user = new User("Bob", 38);
 console.log(user);
+
+const box = document.getElementById("box");
+if (box) {
+  const text = document.createElement("p");
+  text.innerHTML = "I am a text inserted by JavaScript only in the Home page!";
+  box.append(text);
+}
